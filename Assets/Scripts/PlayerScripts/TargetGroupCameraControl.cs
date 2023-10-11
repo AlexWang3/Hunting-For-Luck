12cd8d008f341e018a551a3d4a4c5f291b3a91b4
c62gao <c62gao@uwaterloo.ca>
2023-10-11 01:54:55 -0400

    Target group

--- Assets/Scripts/PlayerScripts/TargetGroupCameraControl.cs
+++ Assets/Scripts/PlayerScripts/TargetGroupCameraControl.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using MetroidvaniaTools;
 
-public class TargetGroupCameraControl : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TargetGroupCameraControl : MonoBehaviour {
+    public Transform Player;
+    public Transform Boss;
+    public CinemachineTargetGroup targetGroup;
+
+    private void Start() {
+        List<CinemachineTargetGroup.Target> targets = new List<CinemachineTargetGroup.Target>();
+        targets.Add(new CinemachineTargetGroup.Target{target = Player, radius = 4, weight = 1});
+        targetGroup.m_Targets = targets.ToArray();
+        LSleep.SleepEvent += AddBossToTargetGroup;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AddBossToTargetGroup() {
+        List<CinemachineTargetGroup.Target> targets = new List<CinemachineTargetGroup.Target>();
+        targets.Add(new CinemachineTargetGroup.Target{target = Player, radius = 3, weight = 3});
+        targets.Add(new CinemachineTargetGroup.Target{target = Boss, radius = 3, weight = 1});
+        targetGroup.m_Targets = targets.ToArray();
     }
 }
